25dbe891601c530825bf82165cbace1e4f855064
titisekas <46317762+titisekas@users.noreply.github.com>
2020-04-12 13:04:20 +0700

    Add files via upload

---
+++ OOP1/src/oop1/Mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop1;
+
+/**
+ *
+ * @author ACER
+ */
+public class Mahasiswa {
+    
+    public static void main(String[] args) {
+      String nama = "Faqih Zada Ikhsan";
+      String kelas = "4A";
+      String nim = "18090127";
+        System.out.println("Nama : "+nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Nim : "+nim);
+        
+    }
+    
+}
